708a3f9f690a836dcbbf9ab31d4434f3386909e6
George <georLeonard@gmail.com>
2018-05-05 01:40:36 -0700

    normalization working, random sampling function also

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import itertools
 from timeit import default_timer as timer
 
-
+# start_nontf = timer()
 # ####################READ IN CSV PARTITION INTO HEAD BODY INPUT OUTPUTS##################
 
 df = pd.read_csv(r'C:\Users\georg\PycharmProjects\Bambi\bambi_test2.csv')
@@ -42,7 +42,7 @@ def rotate(l, x):
     return l[-x:] + l[:-x]
 
 
-def combinatorial_array(mins, maxes, types, resolution = 4, ):
+def perimeter_array(mins, maxes, types, resolution = 4, ):
     """
     takes dataframe of header, pre-pruned, returns a 2-d numpy array of inputs evenly spaced
     over the whole design space accounting for mixtures and categorical variables
@@ -66,12 +66,12 @@ def combinatorial_array(mins, maxes, types, resolution = 4, ):
 
         if cat_bool[i]:
             pair = np.arange(mins[i],maxes[i]+1,1)  # +1 required due to non-inclusive
-            print(pair)
+            # print(pair)
             pairs_arr.append(list(pair))
 
         if cont_bool[i]: #inc mixtures for now
             pair = list()
-            print(mins[i])
+            # print(mins[i])
             a = mins[i]
             b = maxes[i]
             pair.append(a)
@@ -93,13 +93,34 @@ def combinatorial_array(mins, maxes, types, resolution = 4, ):
 
     for i, row in enumerate(itertools.product(*pairs_arr)):
         out_arr[:, i] = row
-
-    return(out_arr)
+    return out_arr
     # for t in itertools.product(*pairs_arr):  ###fast flatten of rows, no needed
     #     print(t)
 
+def design_space_sample(mins, maxes, samples):
+    """
+    For Monte Carlo method to create an average value over the design space using
+    the tensorflow eval, generates array of un-normalized tests
+    ignoring mixture/categorical value constraints
+    """
+    out_arr = np.zeros([len(mins),samples])
+    for i in range(samples):
+        out_arr[:, i] = [np.random.uniform(mins[n],maxes[n]) for n in range(len(mins))]
+    return out_arr
 
-combinatorial_array(mins, maxes, types)
+def normalize(in_arr, norm_vector):
+    """returns normalized vector in same shape as input based on the normalization vector
+    vector length must match input row length"""
+    in_arr = in_arr / norm_vector[:, np.newaxis]  # normalize
+    in_arr = in_arr.transpose()
+    return in_arr
+
+def de_normalize(in_arr, norm_vector):
+    """returns de-normalized array in same shape as input based on the normalization vector
+    vector length must match input row length"""
+    in_arr = in_arr * norm_vector  # normalize
+    # in_arr = in_arr.transpose()
+    return in_arr
 
 num_factors = df_input.shape[1]
 num_outs = df_output.shape[1]
@@ -108,7 +129,7 @@ num_outs = df_output.shape[1]
 # print(num_outs)
 
 #####Generating predictive expression#####
-
+#####Model to fit prediction expression
 sess = tf.InteractiveSession()
 
 x = tf.placeholder(tf.float32, shape = [None,num_factors]) #width of input
@@ -123,7 +144,6 @@ layer5 = tf.layers.dense(layer4, num_factors, tf.nn.softplus, bias_initializer=t
 
 y = tf.layers.dense(layer5, num_outs, tf.nn.softplus)
 
-
 with tf.name_scope('cross_entropy'):
     cross_entropy = tf.losses.huber_loss(labels = y_, predictions = y)
 
@@ -132,21 +152,38 @@ with tf.name_scope('train'):
     # train_step = tf.train.GradientDescentOptimizer(.9).minimize(cross_entropy)
 
 ins = df_input.values
+norm_vector_in = np.linalg.norm(ins, axis = 0, ord = 2)/(ins.shape[1])
 
-norm_vector_in = np.linalg.norm(ins, axis = 0, ord = 1)
 ins = ins / norm_vector_in
 
 outs = df_output.values #pd df to numpy
-norm_vector_out = np.linalg.norm(outs, axis = 0, ord = 1)
+norm_vector_out = np.linalg.norm(outs, axis = 0, ord = 2)/(outs.shape[1])
 outs = outs / norm_vector_out
 
+# print(ins)
 
 
+#####design space optimization model#########
+min_bias = tf.get_variable('min_bias',[num_factors], dtype=tf.float32)
+max_bias = tf.get_variable('max_bias',[num_factors], dtype=tf.float32)
 
-for _ in range(0) :#True:    #do or do not run training
+min_constant = tf.placeholder(tf.float32, shape=[None,num_factors])
+max_constant = tf.placeholder(tf.float32, shape=[None,num_factors])
+
+parim_vals = perimeter_array(mins,maxes,types)
+parim_vals = normalize(parim_vals, norm_vector_in)
+
+# print(type(tf.add(mins,min_bias)))
+# print(tf.add(mins,min_bias).eval())
+# parim_vals2 = perimeter_array(np.add(mins,min_bias),
+#                              np.add(maxes,max_bias),
+#                              types)
+
+# print(parim_vals)
+
+if True:    #do or do not run training
 
     sess.run(tf.global_variables_initializer())
-    # train_i = df_body.shape[1] #length of body = number of runs
 
     test_i = list()
     train_i = list()  ###empty the ins and outs index lists
@@ -160,10 +197,7 @@ for _ in range(0) :#True:    #do or do not run training
 
     loop_start = timer()
 
-    for i in range(50000):
-        # sess.run(train_step, feed_dict={x: np.expand_dims(ins[train_i], axis = 0),
-        #                                 y_: np.expand_dims(outs[train_i], axis = 0)}) #first batch over whole set
-
+    for i in range(5000): #50000):
         sess.run(train_step, feed_dict={x: ins[train_i],
                                         y_: outs[train_i]})
 
@@ -178,21 +212,42 @@ for _ in range(0) :#True:    #do or do not run training
                 else:
                     train_i.append(index)
 
-                # print(test_i)
-                # print(train_i)
-
             train_error = cross_entropy.eval(feed_dict={x: ins[train_i], y_: outs[train_i]})
             test_error = cross_entropy.eval(feed_dict={x: ins[test_i], y_: outs[test_i]})
-
-
 
         if i%500 == 0:
             loop_end = timer()
             delta_t = loop_end - loop_start
             print('step {0}, training error {1}, test error {2} in {3}-seconds'.format(i, train_error, test_error, delta_t))
-            prediction = sess.run(y,{x: ins[:]}) * norm_vector_out
+            # prediction = sess.run(y,{x: ins[:]}) * norm_vector_out
+            # print(ins[:].shape)
+            # print(parim_vals[:])
+    out_weights = [1,2,3]
+
+    perim_eval = sess.run(y,{x: parim_vals[:]}) # * norm_vector_out
+    perim_eval = np.asarray(perim_eval)
+    print(perim_eval)
+    perim_eval = de_normalize(perim_eval,norm_vector_out)
+    print(norm_vector_out)
+    print(norm_vector_in)
+    print(perim_eval)
+
+
+
+
+
+
+    def testprint(perim_eval, out_weights):
+        asdf = perim_eval*out_weights
+        print(asdf)
+
+    # testprint(perim_eval, out_weights)
+    # print(perim_eval*out_weights)
+    # print(perim_eval.shape)
+
+
     # print(prediction)
-            print(100 * (prediction[1]- (outs[1] * norm_vector_out))/(outs[1] * norm_vector_out))
+    #         print(100 * (prediction[1] - (outs[1] * norm_vector_out))/(outs[1] * norm_vector_out))
     # print(prediction[1,1])
     out_weights = [1,1,1]
 
