f056f6bc4688979819dbda3f3e0fbf0b9546a75e
George <georLeonard@gmail.com>
2018-06-02 23:26:29 -0700

    rewrote send_email, now includes support for attachment string / filename

--- email_func.py
+++ email_func.py
@@ -3,7 +3,9 @@ from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
-
+from email import charset
+from email.mime.nonmultipart import MIMENonMultipart
+from email.charset import Charset, BASE64
 from threading import Thread
 import smtplib
 
@@ -15,33 +17,80 @@ MAIL_PASSWORD = 'AkxJVyoDGE5rdVHek09AaWpVyauTldBI2FqOiopiWB/r' #os.environ.get('
 ADMINS = ['highratiotech@gmail.com']
 
 
+file = {
+    'content': u'This,is,a,test,file',
+    'filename': 'testfile.csv'
+}
 
-def send_email(subject, sender, recipients, text_body, html_body, files = None):
 
-    assert isinstance(recipients, list)
-    msg = MIMEMultipart()
-    msg['From'] = sender
-    msg['To'] = COMMASPACE.join(recipients)
-    msg['Date'] = formatdate(localtime=True)
-    msg['Subject'] = subject
+def send_email(send_to, send_from, subject, message_text, file, filename):
+    # Create message container - multi/mixed MIME type for attachment
+    full_email = MIMEMultipart('mixed')
+    full_email['Subject'] = subject
+    full_email['From'] = send_from
+    full_email['To'] = send_to
 
-    msg.attach(MIMEText(text_body))
+    body = MIMEMultipart('alternative')
+    body.attach(MIMEText(message_text.encode('utf-8'),'plain', _charset='utf-8'))
+    body.attach(MIMEText(("""<html>
+                              <head></head>
+                              <body>
+                                <p>""" + message_text + """</p>
+                              </body>
+                            </html>
+                            """).encode('utf-8'), 'html', _charset='utf-8'))
+    full_email.attach(body)
 
-    for f in files or []:
-        with open(f, "rb") as fil:
-            part = MIMEApplication(
-                fil.read(),
-                Name=str(f)
-            )
-        # After the file is closed
-        part['Content-Disposition'] = 'attachment; filename="{}"'.format(f)
-        msg.attach(part)
+    # create the attachment of the message in
+    attachment = MIMENonMultipart('text','csv', charset='utf-8')
+    attachment.add_header('Content-Disposition', 'attachment', filename=filename)
+    cs = Charset('utf-8')
+    cs.body_encoding = BASE64
+    attachment.set_payload(file.encode('utf-8'), charset=cs)
+    full_email.attach(attachment)
 
-    server = smtplib.SMTP(MAIL_SERVER, MAIL_PORT)
-    server.starttls()
-    server.login(MAIL_USERNAME, MAIL_PASSWORD)
-    server.sendmail(sender, recipients, msg.as_string())
-    server.close()
+    # Send the message via SMTP server.
+    s = smtplib.SMTP(MAIL_SERVER, MAIL_PORT)
+    s.starttls()
+    s.login(MAIL_USERNAME, MAIL_PASSWORD)
+    # sendmail function takes 3 arguments: sender's address, recipient's address
+    # and message to send - here it is sent as one string.
+    s.sendmail(send_from, send_to, full_email.as_string())
+    # self.logger.info('email sent')
+    s.quit()
+
+send_email(ADMINS[0],ADMINS[0],'test message new eq', 'your results are enclosed this is message body', 'pretend, this, is a, csv,', 'test.csv')
+
+
+
+
+
+# def send_email(subject, sender, recipients, text_body, html_body, files = None):
+#
+#     assert isinstance(recipients, list)
+#     msg = MIMEMultipart()
+#     msg['From'] = sender
+#     msg['To'] = COMMASPACE.join(recipients)
+#     msg['Date'] = formatdate(localtime=True)
+#     msg['Subject'] = subject
+#
+#     msg.attach(MIMEText(text_body))
+#
+#     for f in files or []:
+#         with open(f, "rb") as fil:
+#             part = MIMEApplication(
+#                 fil.read(),
+#                 Name=str(f)
+#             )
+#         # After the file is closed
+#         part['Content-Disposition'] = 'attachment; filename="{}"'.format(f)
+#         msg.attach(part)
+#
+#     server = smtplib.SMTP(MAIL_SERVER, MAIL_PORT)
+#     server.starttls()
+#     server.login(MAIL_USERNAME, MAIL_PASSWORD)
+#     server.sendmail(sender, recipients, msg.as_string())
+#     server.close()
 
 
 
