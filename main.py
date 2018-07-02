e0094f7eb5099e9b67ba1d65e9cd96c709d79297
George <georLeonard@gmail.com>
2018-07-01 19:14:53 -0700

    issues with errors and tf.sess not closing (due to being out of the loop on try except)

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 from functions import design_space_sample, mix_sum, normalize, cat_ratios, \
     de_normalize, mins_maxes, optimal_design
 from timeit import default_timer as timer
-
+import gc
 # ####################READ IN CSV PARTITION INTO HEAD BODY INPUT OUTPUTS##################
 
 def anal(df, num_requested):
@@ -14,205 +13,216 @@ def anal(df, num_requested):
     :param num_requested
     :return:
     """
-    # print(df)
-    df = df.drop(columns=['Factor Name', ])  # drop experiment run id.
-    df_cols_in_order = df.columns.values.tolist()
-
-    # print(df)
-
-    df_head = df[0:3].reset_index().copy()
-    df_body = df[3:].reset_index().copy()
-    df_body = df_body.apply(pd.to_numeric, errors='ignore')
-
-    # print(df_head)
-
-    df_output = df_body.filter(regex=('OUT.*'))  # selects OUT**** to create DF of outputs
-    df_output = df_output.apply(pd.to_numeric, errors='ignore')
-    df_out_head = df_head.filter(regex=('OUT.*'))  # selects OUT**** to create DF of outputs
-    df_out_head = df_out_head.fillna(value=1)  # sets default value for out weights to 1 if not input by user
-    # print(df_out_head)
-    #
-    # df_input = df_body[df_body.columns.difference(df_output.columns)] #strips outputs to create input DF
-    df_input = df_body.drop(columns=['index', ])
-    df_input = df_input.drop(columns=list(df_output))  # drop output column names
-
-    # print(df_input)
-
-    df_head = df_head.drop(columns=['index', ])
-    df_head = df_head.drop(columns=list(df_output))
-
-    # print(df_head)
-
-    types = df_head.loc[[0]].values.flatten()
-    # experimental mins/maxs from inputted values if not supplied properly or supplied at all
     try:
-        mins = df_head.loc[[1]].values.flatten().astype(np.float32)  # rows to #'s in np array
-        maxes = df_head.loc[[2]].values.flatten().astype(np.float32)
-    except Exception:
-        mins_maxes(df_input.values) ###DEUG ME PLEASE
+        # print(df)
+        df = df.drop(columns=['Factor Name', ])  # drop experiment run id.
+        df_cols_in_order = df.columns.values.tolist()
 
-    labels = df_head.columns.values
-    out_weights = df_out_head.loc[[2]].values.flatten().astype(np.float32)
+        # print(df)
 
-    # print(out_weights)
+        df_head = df[0:3].reset_index().copy()
+        df_body = df[3:].reset_index().copy()
+        df_body = df_body.apply(pd.to_numeric, errors='ignore')
 
-    cat_bool = (types == 'CATEGORICAL') | (types == 'CAT') #needs to be | instead of 'or'
-    cat_encode_by_label = dict()  # nested column -> key -> replace
-    cat_decode_by_label = dict()  #
+        # print(df_head)
 
-    for i, label in enumerate(labels):  # recode all categorical factors to integer numeric
-        if cat_bool[i]:
-            existing = np.unique(df_input[label])
-            replacing = np.arange(0, len(existing))
-            mins[i] = 0
-            maxes[i] = len(existing) - 1
-            cat_encode_by_label[label] = dict(
-                zip(existing, replacing))  # makes full nested dict for later use if needed
-            cat_decode_by_label[label] = dict(zip(replacing, existing))
-            df_input[label] = df_input[label].map(cat_encode_by_label[label])
+        df_output = df_body.filter(regex=('OUT.*'))  # selects OUT**** to create DF of outputs
+        df_output = df_output.apply(pd.to_numeric, errors='ignore')
+        df_out_head = df_head.filter(regex=('OUT.*'))  # selects OUT**** to create DF of outputs
+        df_out_head = df_out_head.fillna(value=1)  # sets default value for out weights to 1 if not input by user
+        # print(df_out_head)
+        #
+        # df_input = df_body[df_body.columns.difference(df_output.columns)] #strips outputs to create input DF
+        df_input = df_body.drop(columns=['index', ])
+        df_input = df_input.drop(columns=list(df_output))  # drop output column names
 
-            # print(df_input)
+        # print(df_input)
 
-            # cat_replaced_by_index[i] =  # generates matching 0 - x range as ndarray to labels dict
-            # cat_replaced_by_index[i] = np.arange(0, len(cat_values_by_index[i])) # generates matching 0 - x range as ndarray to labels dict
+        df_head = df_head.drop(columns=['index', ])
+        df_head = df_head.drop(columns=list(df_output))
 
-    # df_input.replace()
+        # print(df_head)
 
-    # ###mins/maxes might break here for cat values.
-    # df_input.replace(cat_encode_by_label, inplace=True, method = 'ffill')
+        types = df_head.loc[[0]].values.flatten()
+        # experimental mins/maxs from inputted values if not supplied properly or supplied at all
+        try:
+            mins = df_head.loc[[1]].values.flatten().astype(np.float32)  # rows to #'s in np array
+            maxes = df_head.loc[[2]].values.flatten().astype(np.float32)
+        except Exception:
+            mins_maxes(df_input.values) ###DEUG ME PLEASE
 
-    # print(df_input)
+        labels = df_head.columns.values
+        out_weights = df_out_head.loc[[2]].values.flatten().astype(np.float32)
 
-    # asdf = np.unique(df_input[labels[1]]) # all unique entries in a column of data
-    #
-    # print(asdf)
-    # print(type(asdf))
-    num_factors = df_input.shape[1]
-    num_outs = df_output.shape[1]
+        # print(out_weights)
 
-    # ####Generating predictive expression#####
-    # ####Model to fit prediction expression
-    sess = tf.InteractiveSession()
+        cat_bool = (types == 'CATEGORICAL') | (types == 'CAT') #needs to be | instead of 'or'
+        cat_encode_by_label = dict()  # nested column -> key -> replace
+        cat_decode_by_label = dict()  #
 
-    x = tf.placeholder(tf.float32, shape=[None, num_factors])  # width of input
-    y_ = tf.placeholder(tf.float32, shape=[None, num_outs])  # width of output
+        for i, label in enumerate(labels):  # recode all categorical factors to integer numeric
+            if cat_bool[i]:
+                existing = np.unique(df_input[label])
+                replacing = np.arange(0, len(existing))
+                mins[i] = 0
+                maxes[i] = len(existing) - 1
+                cat_encode_by_label[label] = dict(
+                    zip(existing, replacing))  # makes full nested dict for later use if needed
+                cat_decode_by_label[label] = dict(zip(replacing, existing))
+                df_input[label] = df_input[label].map(cat_encode_by_label[label])
 
-    layer1 = tf.layers.dense(x, num_factors, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
-    layer2 = tf.layers.dense(layer1, num_factors, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
-    layer3 = tf.layers.dense(layer2, num_factors, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
-    layer4 = tf.layers.dense(layer3, num_factors, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
-    layer5 = tf.layers.dense(layer4, num_factors, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
+                # print(df_input)
 
-    y = tf.layers.dense(layer5, num_outs, tf.nn.softplus)
+                # cat_replaced_by_index[i] =  # generates matching 0 - x range as ndarray to labels dict
+                # cat_replaced_by_index[i] = np.arange(0, len(cat_values_by_index[i])) # generates matching 0 - x range as ndarray to labels dict
 
-    with tf.name_scope('cross_entropy'):
-        cross_entropy = tf.losses.huber_loss(labels=y_, predictions=y)
+        # df_input.replace()
 
-    with tf.name_scope('train'):
-        train_step = tf.train.AdamOptimizer(learning_rate=.00005, epsilon=.00000001).minimize(cross_entropy)
-        # train_step = tf.train.GradientDescentOptimizer(.9).minimize(cross_entropy)
+        # ###mins/maxes might break here for cat values.
+        # df_input.replace(cat_encode_by_label, inplace=True, method = 'ffill')
 
-    ins_unnorm = df_input.values
+        # print(df_input)
 
-    norm_vector_in = np.linalg.norm(ins_unnorm, axis=0, ord=2) / (ins_unnorm.shape[1])
+        # asdf = np.unique(df_input[labels[1]]) # all unique entries in a column of data
+        #
+        # print(asdf)
+        # print(type(asdf))
+        num_factors = df_input.shape[1]
+        num_outs = df_output.shape[1]
+        import tensorflow as tf  ###down here to prevent memory isseus
 
-    ins = ins_unnorm / norm_vector_in
+        # ####Generating predictive expression#####
+        # ####Model to fit prediction expression
+        sess = tf.InteractiveSession()
 
-    outs = df_output.values  # pd df to numpy
-    norm_vector_out = np.linalg.norm(outs, axis=0, ord=2) / (outs.shape[1])
-    outs = outs / norm_vector_out
+        x = tf.placeholder(tf.float32, shape=[None, num_factors])  # width of input
+        y_ = tf.placeholder(tf.float32, shape=[None, num_outs])  # width of output
 
-    # print(ins)
+        layer1 = tf.layers.dense(x, num_factors, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
+        layer2 = tf.layers.dense(layer1, num_factors, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
+        layer3 = tf.layers.dense(layer2, num_factors, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
+        layer4 = tf.layers.dense(layer3, num_factors, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
+        layer5 = tf.layers.dense(layer4, num_factors, tf.nn.softplus, bias_initializer=tf.random_uniform_initializer)
+
+        y = tf.layers.dense(layer5, num_outs, tf.nn.softplus)
+
+        with tf.name_scope('cross_entropy'):
+            cross_entropy = tf.losses.huber_loss(labels=y_, predictions=y)
+
+        with tf.name_scope('train'):
+            train_step = tf.train.AdamOptimizer(learning_rate=.00005, epsilon=.00000001).minimize(cross_entropy)
+            # train_step = tf.train.GradientDescentOptimizer(.9).minimize(cross_entropy)
+
+        ins_unnorm = df_input.values
+
+        norm_vector_in = np.linalg.norm(ins_unnorm, axis=0, ord=2) / (ins_unnorm.shape[1])
+
+        ins = ins_unnorm / norm_vector_in
+
+        outs = df_output.values  # pd df to numpy
+        norm_vector_out = np.linalg.norm(outs, axis=0, ord=2) / (outs.shape[1])
+        outs = outs / norm_vector_out
+
+        # print(ins)
 
 
-    if True:  # do or do not run training
+        if True:  # do or do not run training
 
-        sess.run(tf.global_variables_initializer())
+            sess.run(tf.global_variables_initializer())
 
-        test_i = list()
-        train_i = list()  ###empty the ins and outs index lists
+            test_i = list()
+            train_i = list()  ###empty the ins and outs index lists
 
-        for index, _ in enumerate(ins):
-            p = np.random.random()
-            if p > 0.8:  # split train / test
-                test_i.append(index)
-            else:
-                train_i.append(index)
+            for index, _ in enumerate(ins):
+                p = np.random.random()
+                if p > 0.8:  # split train / test
+                    test_i.append(index)
+                else:
+                    train_i.append(index)
 
-        loop_start = timer()
+            loop_start = timer()
 
-        for i in range(max(100000, num_factors*10000)):  # 50000):
-            sess.run(train_step, feed_dict={x: ins[train_i],
-                                            y_: outs[train_i]})
+            for i in range(max(100000, num_factors*10000)):  # 50000):
+                sess.run(train_step, feed_dict={x: ins[train_i],
+                                                y_: outs[train_i]})
 
-            if i % 25 == 0 or i == 2 or i == 4 or i == 10:  # batching
-                test_i = list()
-                train_i = list()  ###empty the ins and outs index lists
+                if i % 25 == 0 or i == 2 or i == 4 or i == 10:  # batching
+                    test_i = list()
+                    train_i = list()  ###empty the ins and outs index lists
 
-                for index, _ in enumerate(ins):
-                    p = np.random.random()
-                    if p > 0.8:  # split train / test
-                        test_i.append(index)
-                    else:
-                        train_i.append(index)
+                    for index, _ in enumerate(ins):
+                        p = np.random.random()
+                        if p > 0.8:  # split train / test
+                            test_i.append(index)
+                        else:
+                            train_i.append(index)
 
-                train_error = cross_entropy.eval(feed_dict={x: ins[train_i], y_: outs[train_i]})
-                test_error = cross_entropy.eval(feed_dict={x: ins[test_i], y_: outs[test_i]})
+                    train_error = cross_entropy.eval(feed_dict={x: ins[train_i], y_: outs[train_i]})
+                    test_error = cross_entropy.eval(feed_dict={x: ins[test_i], y_: outs[test_i]})
 
-            if i % 10000 == 0:
-                loop_end = timer()
-                delta_t = loop_end - loop_start
-                print('step {0}, training error {1}, test error {2} in {3}-seconds'.format(i, train_error, test_error,
-                                                                                           delta_t))
+                if i % 10000 == 0:
+                    loop_end = timer()
+                    delta_t = loop_end - loop_start
+                    print('step {0}, training error {1}, test error {2} in {3}-seconds'.format(i, train_error, test_error,
+                                                                                               delta_t))
 
-    # generate samples over design space
-    samples = max(num_factors ** 2 * 1000, 10000000)
+        # generate samples over design space
+        samples = max(num_factors ** 2 * 1000, 10000000)
 
-    space_sample = design_space_sample(mins, maxes, types, samples, mix_sum(ins_unnorm, types))
-    space_sample_norm = normalize(space_sample, norm_vector_in)
-    space_sample_norm = np.transpose(space_sample_norm)
-    # Evaluate samples according to TF model
-    space_sample_eval = sess.run(y, {x: space_sample_norm[:]})
+        space_sample = design_space_sample(mins, maxes, types, samples, mix_sum(ins_unnorm, types))
+        space_sample_norm = normalize(space_sample, norm_vector_in)
+        space_sample_norm = np.transpose(space_sample_norm)
+        # Evaluate samples according to TF model
+        space_sample_eval = sess.run(y, {x: space_sample_norm[:]})
 
-    # weight and sum the resulted models
-    weighted_eval = np.sum(space_sample_eval * out_weights, axis=1)
+        # weight and sum the resulted models
+        weighted_eval = np.sum(space_sample_eval * out_weights, axis=1)
 
-    # select the top X percent or n samples that were evaluated
-    werty, top_index = tf.nn.top_k(weighted_eval, 100000)  # samples//20000) #top 0.5% of samples
-    best_ins = space_sample[:, top_index.eval()]
+        # select the top X percent or n samples that were evaluated
+        werty, top_index = tf.nn.top_k(weighted_eval, 100000)  # samples//20000) #top 0.5% of samples
+        best_ins = space_sample[:, top_index.eval()]
 
-    # generate new mins, maxes based off of monte carlo evaluation above
-    best_mins, best_maxes = mins_maxes(best_ins)
+        # generate new mins, maxes based off of monte carlo evaluation above
+        best_mins, best_maxes = mins_maxes(best_ins)
 
-    # generate ratios for cat variables based off of monte carlo evaluation above
+        # generate ratios for cat variables based off of monte carlo evaluation above
 
-    cat_dict = cat_ratios(best_ins, types, mins, maxes)
+        cat_dict = cat_ratios(best_ins, types, mins, maxes)
 
-    new_experiments = optimal_design \
-        (best_mins, best_maxes, types, num_requested, mix_sum(ins_unnorm, types), norm_vector_in,
-         cat_dict)  # just base mins/maxes right now for debugging!
+        new_experiments = optimal_design \
+            (best_mins, best_maxes, types, num_requested, mix_sum(ins_unnorm, types), norm_vector_in,
+             cat_dict)  # just base mins/maxes right now for debugging!
 
-    out_df = pd.DataFrame(data=np.transpose(new_experiments), columns=df_head.columns.values.tolist())
+        out_df = pd.DataFrame(data=np.transpose(new_experiments), columns=df_head.columns.values.tolist())
 
-    # recode categorical factors:
+        # recode categorical factors:
 
-    for i, label in enumerate(labels):  # recode all categorical factors to integer numeric
-        if cat_bool[i]:
-            # existing = np.unique(df_input[label])  #created above, why duplicate
-            # replacing = np.arange(0, len(existing))
-            mins[i] = 0
-            maxes[i] = len(existing) - 1
+        for i, label in enumerate(labels):  # recode all categorical factors to integer numeric
+            if cat_bool[i]:
+                # existing = np.unique(df_input[label])  #created above, why duplicate
+                # replacing = np.arange(0, len(existing))
+                mins[i] = 0
+                maxes[i] = len(existing) - 1
 
-            # cat_encode_by_label[label] = dict(zip(existing, replacing))  # makes full nested dict for later use if needed
-            # cat_decode_by_label[label] = dict(zip(replacing, existing)) #created above
-            df_input[label] = df_input[label].map(cat_decode_by_label[label])
+                # cat_encode_by_label[label] = dict(zip(existing, replacing))  # makes full nested dict for later use if needed
+                # cat_decode_by_label[label] = dict(zip(replacing, existing)) #created above
+                df_input[label] = df_input[label].map(cat_decode_by_label[label])
 
-    df = pd.DataFrame(columns=df_cols_in_order)  # only return newly generated experiments #comment me out if going for full array inc. data
-    out_df = df.append(out_df, sort=False)  # df set at top to be full frame minus experiment #
-    out_df = out_df[df_cols_in_order]
-    out_df = out_df.reset_index(drop=True)
-    sess.close()
-    return out_df
+        df = pd.DataFrame(columns=df_cols_in_order)  # only return newly generated experiments #comment me out if going for full array inc. data
+        out_df = df.append(out_df, sort=False)  # df set at top to be full frame minus experiment #
+        out_df = out_df[df_cols_in_order]
+        out_df = out_df.reset_index(drop=True)
+        sess.close()
+        sess.__del__() #nuke session
+        gc.collect()
+        return out_df
+
+    except Exception as e:
+        sess.close()
+        sess.__del__()  # nuke session
+        gc.collect()
+        return e
+
 
 
 ##test eval##
--- pullpush.py
+++ pullpush.py
@@ -35,6 +35,8 @@ db = create_engine(SQLALCHEMY_DATABASE_URI)
 
 
 while True:
+    data, user_id, num_requested, id_, project_label, user_email_address = None, None, None, None, None, None  # reset variables so they don't carry over  # call master function on data here
+
     try:
         SQLALCHEMY_DATABASE_URI = subprocess.Popen(r'heroku config:get DATABASE_URL -a gumdrop', shell=True,
                                                    stdout=subprocess.PIPE)
@@ -107,8 +109,8 @@ while True:
 
     try:
         out_df = anal(in_df, num_requested)
-    except:
-        print('FORMATTING ERROR Analysis of id# {} failed at {}'.format(id_, datetime.utcnow()))
+    except Exception as e:
+        print('ERROR in Analysis of id# {} failed at {} -- {}'.format(id_, datetime.utcnow(),e))
         # db.execute("""
         # UPDATE project_index
         # SET analysis_in_progress = FALSE,
@@ -117,13 +119,16 @@ while True:
         # WHERE id = {}
         # """.format(datetime.utcnow(), id_))
         #will consider in_progress/not complete error signal in evaluation
+
         message_text = """ Sorry but there appears to be a formatting error with project:{}.  
         This error occured at {} UTC. 
         Please double check your inputs against the instructions provided in the template and resubmit.
+        
+        If that does not solve the issue please contact support@highratiotech.com
         """.format(project_label, datetime.utcnow())
-        send_from = 'highratiotech@gmail.com'
+        send_from = 'noreply@highratiotech.com'
         subject = 'Analysis of Project:{} Failed Formatting Error.'.format(project_label)
-        user_email_address = 'georLeonard@gmail.com'  # overwrite for testing purposes!!!
+        # user_email_address = user_email_address
 
         try:
             send_email(user_email_address, send_from, subject,
@@ -138,51 +143,65 @@ while True:
                 q = None  # does nothing...
         continue
 
-    out_JSON = out_df.to_json(double_precision=15, orient='split')
-    # out_df.to_json(r'C:\Users\georg\PycharmProjects\Bambi\test.json', orient='split')  #save local copy for eval
-    out_csv = out_df.to_csv()
-    # print(datetime.utcnow())
-    # print(str(datetime.utcnow()))
-
-    # print(out_JSON)
-    # print(out_df)
-
-
-
-    db.execute("""
-    UPDATE project_index
-    SET analysis_in_progress = FALSE,
-    analysis_complete = TRUE,
-    timestamp_updated = '{}',
-    results = '{}'
-    WHERE id = {}
-    """.format(datetime.utcnow(), out_JSON, id_))
-
-    message_text = """ 
-    Please find a csv including your data with {} suggested experiments 
-    Your data and results can be viewed at <PLACEHOLDER URL GOES HERE>""".format(num_requested)
-
-    send_from = 'highratiotech@gmail.com'
-    subject = 'Analysis of Project:{} completed.'.format(project_label)
-    filename = '{}_{}.csv'.format(project_label, num_requested)
-    user_email_address = 'georLeonard@gmail.com'  # overwrite for testing purposes!!!
-
     try:
-        send_email(user_email_address, send_from, subject,
-                   message_text, file=out_csv, filename=filename)
-    except Exception as e:
-        print(e)
-        sleep(10)
-        try:  # second attempt to send
+        out_JSON = out_df.to_json(double_precision=15, orient='split')
+    # out_df.to_json(r'C:\Users\georg\PycharmProjects\Bambi\test.json', orient='split')  #save local copy for eval
+        out_csv = out_df.to_csv()
+        # print(datetime.utcnow())
+        # print(str(datetime.utcnow()))
+
+        # print(out_JSON)
+        # print(out_df)
+
+
+
+        db.execute("""
+        UPDATE project_index
+        SET analysis_in_progress = FALSE,
+        analysis_complete = TRUE,
+        timestamp_updated = '{}',
+        results = '{}'
+        WHERE id = {}
+        """.format(datetime.utcnow(), out_JSON, id_))
+
+        message_text = """ 
+        Please find a csv including your data with {} suggested experiments 
+        Your data and results can be viewed at <PLACEHOLDER URL GOES HERE>""".format(num_requested)
+
+        send_from = 'highratiotech@gmail.com'
+        subject = 'Analysis of Project:{} completed.'.format(project_label)
+        filename = '{}_{}.csv'.format(project_label, num_requested)
+        user_email_address = 'georLeonard@gmail.com'  # overwrite for testing purposes!!!
+
+        try:
             send_email(user_email_address, send_from, subject,
                        message_text, file=out_csv, filename=filename)
-        except:
-            q = None  # does nothing...
+        except Exception as e:
+            print(e)
+            sleep(10)
+            try:  # second attempt to send
+                send_email(user_email_address, send_from, subject,
+                           message_text, file=out_csv, filename=filename)
+            except:
+                q = None  # does nothing...
 
-    # send_email('georLeonard@gmail.com','georLeonard@gmail.com','asdf','test')
-    print('Analysis of id# {} complete at {}'.format(id_, datetime.utcnow()))
+        # send_email('georLeonard@gmail.com','georLeonard@gmail.com','asdf','test')
+        print('Analysis of id# {} complete at {}'.format(id_, datetime.utcnow()))
 
-    data, user_id, num_requested, id_, project_label, user_email_address = None, None, None, None, None, None  # reset variables so they don't carry over  # call master function on data here
+
+    except Exception as e:
+        print('ERROR in Analysis of id# {} failed at {} -- {}'.format(id_, datetime.utcnow(), e))
+
+
+
+    # db.execute("""
+    # UPDATE project_index
+    # SET analysis_in_progress = FALSE,
+    # analysis_complete = TRUE,
+    # timestamp_updated = '{}',
+    # WHERE id = {}
+    # """.format(datetime.utcnow(), id_))
+    # will consider in_progress/not complete error signal in evaluation
 
     # print(type(out_csv))
     # print(type(out_JSON))
