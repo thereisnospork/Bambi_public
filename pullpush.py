782801654c497dcb9c1c15392fa15d4f7b86dd3b
George <georLeonard@gmail.com>
2018-07-05 01:46:31 -0700

    added error message update to db and sends(???) email with results

--- pullpush.py
+++ pullpush.py
@@ -11,11 +11,11 @@ from email_func import send_email
 ###CONFIG###
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-wi4ll-never-guess'
 
-
 # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:test1234@127.0.0.1:5432/gumdrop'
 
-SQLALCHEMY_DATABASE_URI = subprocess.Popen(r'heroku config:get DATABASE_URL -a gumdrop', stdout=subprocess.PIPE, shell=True)
+SQLALCHEMY_DATABASE_URI = subprocess.Popen(r'heroku config:get DATABASE_URL -a gumdrop', stdout=subprocess.PIPE,
+                                           shell=True)
 SQLALCHEMY_DATABASE_URI.wait()
 SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.stdout.read().decode('utf-8')
 # SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI + r'?ssl=true'
@@ -49,20 +49,19 @@ while True:
         sleep(180)
         continue
 
-
     try:
-            data_conn = db.execute(
+        data_conn = db.execute(
             """
             SELECT data, user_id, num_requested, id, project_label FROM project_index 
             WHERE (analysis_in_progress = FALSE AND 
             analysis_complete = FALSE) 
             ORDER BY timestamp_created ASC 
             LIMIT 1""")
-        #
-        # data_conn = db.execute(
-        #     """
-        #     SELECT data, user_id, num_requested, id FROM project_index
-        #     """)
+    #
+    # data_conn = db.execute(
+    #     """
+    #     SELECT data, user_id, num_requested, id FROM project_index
+    #     """)
     except:
         sleep(5)
         continue
@@ -110,15 +109,14 @@ while True:
     try:
         out_df = anal(in_df, num_requested)
     except Exception as e:
-        print('ERROR in Analysis of id# {} failed at {} -- {}'.format(id_, datetime.utcnow(),e))
-        # db.execute("""
-        # UPDATE project_index
-        # SET analysis_in_progress = FALSE,
-        # analysis_complete = TRUE,
-        # timestamp_updated = '{}',
-        # WHERE id = {}
-        # """.format(datetime.utcnow(), id_))
-        #will consider in_progress/not complete error signal in evaluation
+        print('ERROR in Analysis of id# {} failed at {} -- {}'.format(id_, datetime.utcnow(), e))
+        db.execute("""
+        UPDATE project_index
+        SET error = True,
+        timestamp_updated = '{}',
+        WHERE id = {}
+        """.format(datetime.utcnow(), id_))
+        # will consider in_progress/not complete error signal in evaluation
 
         message_text = """ Sorry but there appears to be a formatting error with project:{}.  
         This error occured at {} UTC. 
@@ -145,15 +143,13 @@ while True:
 
     try:
         out_JSON = out_df.to_json(double_precision=15, orient='split')
-    # out_df.to_json(r'C:\Users\georg\PycharmProjects\Bambi\test.json', orient='split')  #save local copy for eval
+        # out_df.to_json(r'C:\Users\georg\PycharmProjects\Bambi\test.json', orient='split')  #save local copy for eval
         out_csv = out_df.to_csv()
         # print(datetime.utcnow())
         # print(str(datetime.utcnow()))
 
         # print(out_JSON)
         # print(out_df)
-
-
 
         db.execute("""
         UPDATE project_index
@@ -168,10 +164,10 @@ while True:
         Please find a csv including your data with {} suggested experiments 
         Your data and results can be viewed at <PLACEHOLDER URL GOES HERE>""".format(num_requested)
 
-        send_from = 'highratiotech@gmail.com'
+        send_from = 'noreply@highratiotech.com'
         subject = 'Analysis of Project:{} completed.'.format(project_label)
         filename = '{}_{}.csv'.format(project_label, num_requested)
-        user_email_address = 'georLeonard@gmail.com'  # overwrite for testing purposes!!!
+        # user_email_address = 'georLeonard@gmail.com'  # overwrite for testing purposes!!!
 
         try:
             send_email(user_email_address, send_from, subject,
@@ -182,8 +178,13 @@ while True:
             try:  # second attempt to send
                 send_email(user_email_address, send_from, subject,
                            message_text, file=out_csv, filename=filename)
-            except:
-                q = None  # does nothing...
+            except Exception:
+                sleep(30)
+            try:
+                send_email(user_email_address, send_from, subject,
+                           message_text, file=out_csv, filename=filename)
+            except Exception:
+                pass  # does nothing...
 
         # send_email('georLeonard@gmail.com','georLeonard@gmail.com','asdf','test')
         print('Analysis of id# {} complete at {}'.format(id_, datetime.utcnow()))
@@ -191,8 +192,6 @@ while True:
 
     except Exception as e:
         print('ERROR in Analysis of id# {} failed at {} -- {}'.format(id_, datetime.utcnow(), e))
-
-
 
     # db.execute("""
     # UPDATE project_index
